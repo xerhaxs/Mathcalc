23573652b0525066a2329ce92eaf3090572a2949
xerhaxs <xerhaxs@protonmail.com>
2023-04-07 17:09:35 +0200

    Reordering

--- Mathcalc/src/Code/Main.java
+++ Mathcalc/src/Code/Main.java
@@ -4,7 +4,7 @@ import GUI.CalcGUI;
 
 import javax.swing.*;
 
-public class Main {
+public class    Main {
     public static void main(String [] args) {
         JFrame frame = new JFrame("Calc");
         frame.setContentPane(new CalcGUI().panelMain);
